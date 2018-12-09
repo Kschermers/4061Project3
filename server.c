b434e5854a6ef2bc77b0db9ba9e5b107126e34ec
Samuel Ball <ballx188@umn.edu>
2018-12-09 15:09:21 -0600

    merge

--- server.c
+++ server.c
@@ -308,7 +308,7 @@ void * worker(void *arg) {
 
 
     // TODO Log the request into the file and terminal
-    snprintf(log_str, "[%d][%d][%d][%s][%s][%dms][%s]",
+    snprintf(log_str, 256, "[%d][%d][%d][%s][%s][%dms][%s]",
              thread_id, req_num, current_req.fd,
              current_req.request, bytes_error, elapsed, cache_hit_miss);
     int log_len = strlen(log_str);
